21d08a5ccfd9b96782626b69a16f74bd2a9ec935
kamalogudah <mcpaul2058@gmail.com>
2019-12-14 12:53:20 +0300

    Wiring upan Action Creator

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from './../actions';
 
 class PostList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return (
       <div className="ui container"> Post List </div>);
@@ -9,4 +15,8 @@ class PostList extends Component {
   }
 }
 
-export default PostList;
+const mapPropToState = (state) => {
+  return { }
+}
+
+export default connect(mapPropToState, { fetchPosts})(PostList);
