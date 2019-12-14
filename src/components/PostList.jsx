24c7e0dfa5b4de326debdb3666d8bcd977d694f4
kamalogudah <mcpaul2058@gmail.com>
2019-12-14 12:38:52 +0300

    A touch more setup

---
+++ src/components/PostList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+  render() {
+    return (
+      <div className="ui container"> Post List </div>);
+
+
+  }
+}
+
+export default PostList;
