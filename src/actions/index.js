9f42f363cc0622979f58a052eb02ecd18b1606cb
kamalogudah <mcpaul2058@gmail.com>
2019-12-14 15:23:40 +0300

    Redux Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceHolder.get('/posts');
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
-  };
+export const fetchPosts = () => {
+  return async (dispatch) => {
+    const response = await jsonPlaceHolder.get('/posts');
+
+    dispatch({
+      type: 'FETCH_POSTS',
+      payload: response
+    });
+  }
 };
